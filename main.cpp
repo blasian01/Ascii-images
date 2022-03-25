b44377bc9af780867059163a66e20b8e53653fab
blasian01 <67053548+blasian01@users.noreply.github.com>
2022-03-25 02:05:48 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+using namespace std;
+
+int main() 
+{
+    //intalizing variables
+    int n;
+
+    //getting input from user
+    cout << "Please tnter a number: ";
+    cin >> n;
+    return 0;
+}
