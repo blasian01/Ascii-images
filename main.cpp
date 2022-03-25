4bd56f4c39dcd6c65463035c45cb2fdbb253a790
blasian01 <67053548+blasian01@users.noreply.github.com>
2022-03-25 02:12:28 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream> 
 using namespace std;
 
+//testing to see if this git thing works 
 int main() 
 {
     //intalizing variables
     int n;
 
     //getting input from user
-    cout << "Please tnter a number: ";
+    cout << "Please enter a number: ";
     cin >> n;
     return 0;
 }
