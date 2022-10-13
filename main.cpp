ad9a7ed8ea56e71bcba05fe8737eb7b68f299f58
blasian01 <67053548+blasian01@users.noreply.github.com>
2022-10-13 03:48:57 -0500

    psudo code and removing unwanted files

--- main.cpp
+++ main.cpp
@@ -7,10 +7,54 @@ using namespace std;
 int main() 
 {
     //intalizing variables
-    int n;
+    const string density = "Ñ@#W$9876543210?!abc;:+=-,._ "; //acii to represent the pixel density
+    ifstream image(".jpg"); 
+    int height, width
+    int r, g, b, grey;
 
-    //getting input from user
-    cout << "Please enter a number: ";
-    cin >> n;
+    string filePath;
+    string exportPath;
+
+
+    // asking user to type in the file path to the photo they want to convert to ascii
+    do
+    {
+        cout << "Please enter a file path to a photo: ";
+        cin >> filePath;
+    }
+    while(filePath == true);  // if the path does not lead to a photo then ask the user to try again
+
+
+    //ask user for a directory that they would like theyre photo to be exported to
+    do
+    {
+        cout << "Please enter a file path to export to: ";
+        cin >> exportPath;
+    }
+    while(exportPath == true);  // if the path does not lead to a photo then ask the user to try again
+
+
+    //reading the pixels from the image that is selected
+    for(int i = 0; i < positionX; i++)
+    {
+        for(int k = 0; k < positionY; k++)
+        {
+
+        }
+    }
+
+    //converting pixel value from rgb to grey scale for brightness 
+
+
+    // replacing pixels with ascii 
+    
+    
+    //rendering new image
+
+
+    //pasting new image to a selcted directory
+
+    cout << "Success" << endl;
+
     return 0;
 }
